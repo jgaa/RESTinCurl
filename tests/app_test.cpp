252cb0bc1b506cad5f90024f36c4dcca56af46c8
Jarle Aase <jarle@jgaa.com>
2018-09-02 23:34:39 +0300

    Added simple example/test

---
+++ tests/app_test.cpp
@@ -0,0 +1,32 @@
+
+/* Simple application using the library.
+ *
+ * Just to get an idea about the size of the
+ * binary the library produce.
+ */
+
+#include "restincurl/restincurl.h"
+
+using namespace std;
+using namespace restincurl;
+
+int main( int argc, char * argv[]) {
+
+    restincurl::Client client;
+    string data;
+    restincurl::InDataHandler<std::string> data_handler(data);
+
+    client.Build()->Get("http://localhost:3001/normal/manyposts")
+        .AcceptJson()
+        .StoreData(data)
+        .Header("X-Client", "restincurl")
+        .WithCompletion([&](auto result) {
+            clog << "In callback!" << endl;
+        })
+        .Execute();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    client.CloseWhenFinished();
+    client.WaitForFinish();
+}
