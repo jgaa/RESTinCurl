a5b080f4cf331a9897779c12d0e34893c4a89ae7
Jarle Aase <jarle@jgaa.com>
2018-09-03 14:11:52 +0300

    Added license

--- include/restincurl/restincurl.h
+++ include/restincurl/restincurl.h
@@ -1,18 +1,44 @@
 #pragma once
 
+/*
+    MIT License
+
+    Copyright (c) 2018 Jarle Aase
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+    On Github: https://github.com/jgaa/RESTinCurl
+*/
+
+#include <atomic>
+#include <chrono>
+#include <deque>
 #include <exception>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <map>
 #include <memory>
+#include <mutex>
 #include <string>
 #include <thread>
-#include <functional>
-#include <chrono>
 #include <vector>
-#include <deque>
-#include <iterator>
-#include <iostream>
-#include <mutex>
-#include <map>
-#include <atomic>
 
 #include <assert.h>
 #include <sys/select.h>
